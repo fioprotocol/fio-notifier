debb0df0087baf9d7cf8d438ea127c0a79ae0946
pawmmm <pawelmas@gmail.com>
2024-08-27 13:03:51 -0600

    Switched to using deltas on domain table

--- lambda/app.ts
+++ lambda/app.ts
@@ -1,20 +1,46 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'; import * as AWS from 'aws-sdk'; import axios from 'axios';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+import axios from 'axios';
 const s3 = new AWS.S3();
 const bucketName = process.env.S3_BUCKET_NAME;
 const apiUrl = process.env.API_URL;
-const maxBlocksPerRun = parseInt(process.env.MAX_BLOCKS_PER_RUN || '1000', 10);
 const discordWebhookUrl = process.env.DISCORD_WEBHOOK_URL;
+
+interface Delta {
+    timestamp: string;
+    present: number;
+    code: string;
+    scope: string;
+    table: string;
+    primary_key: string;
+    payer: string;
+    block_num: number;
+    block_id: string;
+    data: {
+        id: string;
+        name: string;
+        domainhash: string;
+        account: string;
+        is_public: number;
+        expiration: string;
+    };
+}
+
+interface DeltaResponse {
+    query_time_ms: number;
+    last_indexed_block: number;
+    last_indexed_block_time: string;
+    total: {
+        value: number;
+        relation: string;
+    };
+    deltas: Delta[];
+}
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
-        if (!bucketName) {
-            throw new Error('S3_BUCKET_NAME environment variable is not set');
-        }
-        if (!apiUrl) {
-            throw new Error('API_URL environment variable is not set');
-        }
-        if (!discordWebhookUrl) {
-            throw new Error('DISCORD_WEBHOOK_URL environment variable is not set');
+        if (!bucketName || !apiUrl || !discordWebhookUrl) {
+            throw new Error('Required environment variables are not set');
         }
 
         // Read data.json from S3
@@ -24,7 +50,6 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         };
         const data = await s3.getObject(params).promise();
 
-        // Check if data.Body is defined
         if (!data.Body) {
             throw new Error('Empty data received from S3');
         }
@@ -32,7 +57,6 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
 
         console.log(`S3 active flag: ${jsonData.active}`);
 
-        // Check if the last Lambda function is still running
         if (jsonData.active) {
             console.log('Lambda function still running, exiting.');
             return {
@@ -43,67 +67,56 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
             };
         }
 
-        // Set the active flag to true so that next Lambda function can exit early if it detects it's still running
+        // Set the active flag to true
         jsonData.active = true;
         await s3.putObject({
             Bucket: bucketName,
             Key: 'data.json',
             Body: JSON.stringify(jsonData),
         }).promise();
-        let lastBlock = jsonData.last_block;
-        let lastIrreversibleBlock = 0;
-        console.log('Staring processing at block '+(lastBlock+1));
 
-        // Make the initial API call to get the last irreversible block
-        const initialResponse = await axios.post(`${apiUrl}/v1/history/get_block_txids`, {
-            block_num: lastBlock + 1,
-        });
-        lastIrreversibleBlock = initialResponse.data.last_irreversible_block;
-        const maxBlockNum = Math.min(lastBlock + maxBlocksPerRun, lastIrreversibleBlock);
-        const registeredFioDomains: string[] = [];
-        const burnedFioDomains: string[] = [];
+        let lastBlock = jsonData.last_block + 1;
+        console.log('Starting processing at block ' + lastBlock);
 
-        while (lastBlock < maxBlockNum) {
-            // Fetch transaction IDs for each block in range
-            const response = await axios.post(`${apiUrl}/v1/history/get_block_txids`, {
-                block_num: lastBlock + 1,
-            });
-            const actionIds = response.data.ids;
-
-            if (actionIds && actionIds.length > 0) {
-                // If block has transactions process each
-                for (const actionId of actionIds) {
-                    // Make the API call to get the transaction details
-                    const transactionResponse = await axios.post(`${apiUrl}/v1/history/get_transaction`, {
-                        id: actionId,
-                    });
-                    const traces = transactionResponse.data.traces;
-
-                    for (const trace of traces) {
-                        if (
-                            trace.action_ordinal === 1 &&
-                            trace.act.account === 'fio.address' &&
-                            trace.act.name === 'regdomain'
-                        ) {
-                            registeredFioDomains.push(trace.act.data.fio_domain);
-                        } else if (trace.act.account === 'fio.address' && trace.act.name === 'burndomain') {
-                            burnedFioDomains.push(trace.act.data.domainname);
-                        }
-                    }
-                }
+        // Get domains table deltas
+        const response = await axios.get<DeltaResponse>(`${apiUrl}/v2/history/get_deltas`, {
+            params: {
+                code: 'fio.address',
+                scope: 'fio.address',
+                table: 'domains',
+                sort: 'asc',
+                after: lastBlock
             }
-            lastBlock++;
+        });
+
+        // Look for registrations and burns
+        const registeredFioDomains: { [key: string]: boolean } = {};
+        const burnedFioDomains: { [key: string]: boolean } = {};
+
+        for (const delta of response.data.deltas) {
+            if (delta.present === 1) {
+                registeredFioDomains[delta.data.name] = true;
+            } else if (delta.present === 0) {
+                burnedFioDomains[delta.data.name] = true;
+            }
+
+            lastBlock = Math.max(lastBlock, delta.block_num);
+        }
+
+        // If no new deltas were found, update lastBlock to last_indexed_block
+        if (response.data.deltas.length === 0) {
+            lastBlock = response.data.last_indexed_block;
         }
 
         // Send notifications to Discord webhook
-        if (registeredFioDomains.length > 0) {
-            const message = `The following FIO Domains were registered recently: ${registeredFioDomains.join(', ')}`;
-            await axios.post(discordWebhookUrl, {content: message});
+        if (Object.keys(burnedFioDomains).length > 0) {
+            const message = `The following FIO Domains were recently burned: ${Object.keys(burnedFioDomains).join(', ')}`;
+            await axios.post(discordWebhookUrl, { content: message });
         }
 
-        if (burnedFioDomains.length > 0) {
-            const message = `The following FIO Domains were burned recently: ${burnedFioDomains.join(', ')}`;
-            await axios.post(discordWebhookUrl, {content: message});
+        if (Object.keys(registeredFioDomains).length > 0) {
+            const message = `The following FIO Domains were recently registered/renewed: ${Object.keys(registeredFioDomains).join(', ')}`;
+            await axios.post(discordWebhookUrl, { content: message });
         }
 
         // Update data.json in S3 with the new last_block value
@@ -115,7 +128,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
             Body: JSON.stringify(jsonData),
         }).promise();
 
-        console.log('Processing completed at block '+lastBlock);
+        console.log('Processing completed at block ' + lastBlock);
 
         return {
             statusCode: 200,
@@ -154,4 +167,4 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
             }),
         };
     }
-}
+};
